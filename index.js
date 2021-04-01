e2e5b98456c87159deede28d60c2f0802fed3c52
johnny <johnny1194@hotmail.com>
2021-04-01 18:22:25 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+const num1 =31;
+const num2=2;
+const multiply = num1*num2;
 
-// Write your code here
+let random = Math.floor(Math.random()*10)+1;
+const num3=10;
+const num4=6;
+let mod = num3-num4;
+let max = Math.max (1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
